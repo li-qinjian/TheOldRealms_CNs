c408865f2d817eecd3671ac8f6a670f0892baa64
西米西正 <ximixizheng@icloud.com>
2025-01-16 14:47:26 +0800

    1

---
+++ Modules/TheOldRealms_CNs/ModuleData/tor_strings.xslt
@@ -0,0 +1,12 @@
+﻿<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+    <xsl:output omit-xml-declaration="yes"/>
+    <xsl:template match="@*|node()">
+        <xsl:copy>
+            <xsl:apply-templates select="@*|node()"/>
+        </xsl:copy>
+    </xsl:template>
+
+	<xsl:template match="string[@id='str_customresource_trade_label']/@text">
+		<xsl:attribute name="text">{=q3bhAMpk}You will {?PAY_OR_GET}get{?}pay{\?} {TRADE_AMOUNT}"</xsl:attribute>
+	</xsl:template>
+</xsl:stylesheet>
---
+++ TheOldRealms_CNs/Patches/CustomResourcePatchesPatch.cs
@@ -0,0 +1,48 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Text;
+using TaleWorlds.Localization;
+using TOR_Core.AbilitySystem.Spells.Prayers;
+using TOR_Core.CampaignMechanics.CustomResources;
+using TOR_Core.HarmonyPatches;
+
+namespace TheOldRealms_CNs.Patches
+{
+    [HarmonyPatch]
+    internal class CustomResourcePatchesPatch
+    {
+        [HarmonyTranspiler]
+        [HarmonyPatch(typeof(CustomResourcePatches), "AdditionalPartyScreenClosingLogic")]
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions /*, ILGenerator generator*/)
+        {
+            var NamePropertyGetter = AccessTools.PropertyGetter(typeof(CustomResource), "Name");
+            var LocalizedNameGetter = AccessTools.PropertyGetter(typeof(CustomResource), "LocalizedName");
+
+            var found = false;
+            foreach (var instruction in instructions)
+            {
+                if (instruction.opcode == OpCodes.Ldstr && instruction.operand.ToString() == "You don't have enough ")
+                {
+                    yield return new CodeInstruction(OpCodes.Ldstr, "{=rSRZwwck}You don't have enough ");
+                    yield return new CodeInstruction(OpCodes.Ldnull);
+                    yield return new CodeInstruction(OpCodes.Newobj, AccessTools.Constructor(typeof(TextObject), new Type[] { typeof(string), typeof(Dictionary<string, object>) }));
+                    yield return new CodeInstruction(OpCodes.Callvirt, AccessTools.Method(typeof(Object), nameof(Object.ToString)));
+                    found = true;
+                }
+                else if (instruction.opcode == OpCodes.Callvirt && instruction.operand == (object)NamePropertyGetter)
+                {
+                    yield return new CodeInstruction(OpCodes.Callvirt, LocalizedNameGetter);
+                    yield return new CodeInstruction(OpCodes.Callvirt, AccessTools.Method(typeof(Object), nameof(Object.ToString)));
+                }
+                else
+                {
+                    yield return instruction;
+                }
+            }
+            if (found is false)
+                throw new ArgumentException("Cannot find ldstr in CustomResourcePatches.AdditionalPartyScreenClosingLogic");
+        }
+    }
+}
